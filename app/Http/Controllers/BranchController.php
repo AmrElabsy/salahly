f7dae3bfa3dfdc588091a4bc9aabeb548003221a
AmrElabsy <amrelabsy55@gmail.com>
2023-02-18 16:50:04 +0200

    Remove Comments for Branches

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -8,33 +8,17 @@ use App\Http\Requests\UpdateBranchRequest;
 
 class BranchController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $branches = Branch::all();
         return view("branches.index", compact("branches"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view("branches.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreBranchRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreBranchRequest $request)
     {
         $branch = new Branch();
@@ -44,35 +28,16 @@ class BranchController extends Controller
         return redirect()->route("branch.index")->withStatus(__("titles.branch_added"));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Branch  $branch
-     * @return \Illuminate\Http\Response
-     */
     public function show(Branch $branch)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Branch  $branch
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Branch $branch)
     {
         return view("branches.edit", compact("branch"));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateBranchRequest  $request
-     * @param  \App\Models\Branch  $branch
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateBranchRequest $request, Branch $branch)
     {
         $branch->name = $request->get("name");
@@ -80,16 +45,9 @@ class BranchController extends Controller
         return redirect()->route("branch.index")->withStatus(__("titles.branch_updated"));
         }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Branch  $branch
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Branch $branch)
     {
         $branch->delete();
         return redirect()->route("branch.index")->withStatus(__("titles.branch_deleted"));
-    
     }
 }
