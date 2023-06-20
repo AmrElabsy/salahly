8173e2061ae8691664adf74c752a5b97b5c7c3d3
AmrElabsy <amrelabsy55@gmail.com>
2023-06-20 03:32:29 +0300

    Fix Typo

--- app/Services/ProblemService.php
+++ app/Services/ProblemService.php
@@ -57,7 +57,7 @@
             $result = [];
             foreach ($materials as $material) {
                 $material = Material::find($material);
-                $result[$material->id] = ["price" => $material->price];
+                $result[$material->id] = ["price" => $material->price->price];
             }
 
             return $result;
