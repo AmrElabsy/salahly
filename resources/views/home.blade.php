86f6bde6387e3dbcad3813eb764f51cecf608868
belal <belal@gmail.com>
2023-08-05 18:43:53 +0300

    Home page Color Buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,26 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <a href="{{route('problem.create')}}" class="btn btn-primary btn-lg btn-block">إستلام جهاز جديد</a>
+            <div class="col-md-4" >
+                <a href="{{route('problem.create')}}" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">إستلام جهاز جديد</a>
             </div>
             <div class="col-md-4">
-                <a href="{{route('problem.index')}}" class="btn btn-primary btn-lg btn-block">اجهزة تحت الكشف</a>
+                <a href="{{route('problem.index')}}" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">اجهزة تحت الكشف</a>
             </div>
             <div class="col-md-4">
-                <a href="{{route('attendance.index')}}" class="btn btn-primary btn-lg btn-block">الحضور والغياب</a>
+                <a href="{{route('attendance.index')}}" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">الحضور والغياب</a>
             </div>
         </div>
         <br>
         <div class="row">
             <div class="col-md-4">
-                <a href="#" class="btn btn-primary btn-lg btn-block">تم التسليم</a>
+                <a href="#" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">تم التسليم</a>
             </div>
             <div class="col-md-4">
-                <a href="#" class="btn btn-primary btn-lg btn-block">الرفض</a>
+                <a href="#" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">الرفض</a>
             </div>
             <div class="col-md-4">
-                <a href="#" class="btn btn-primary btn-lg btn-block"> الخزنة </a>
+                <a href="#" class="btn btn-dark btn-lg btn-block" style="height: 100px; display: flex; align-items: center; justify-content: center;">الخزنة</a>
             </div>
         </div>
     </div>
