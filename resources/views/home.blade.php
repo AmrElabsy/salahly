2710d75a88fe2ed131496feb5dc86164d244b2e4
AmrElabsy <amrelabsy55@gmail.com>
2023-08-05 20:00:08 +0300

    Modify Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,14 @@
         <div class="row">
 
 
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <a href="{{route('problem.create')}}" class="btn btn-primary btn-lg btn-block">{{__('titles.new_device')}}</a>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <a href="{{route('problem.index')}}" class="btn btn-primary btn-lg btn-block">{{ __('titles.devices_under_detection') }}</a>
+            </div>
+            <div class="col-md-4">
+                <a href="{{route('attendance.index')}}" class="btn btn-primary btn-lg btn-block">الحضور والغياب</a>
             </div>
         </div>
         <br>
